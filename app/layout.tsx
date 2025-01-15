223071c39fd44a1f5024b32708c9b19f87cfe249
hens-msn <hendymamusung@outlook.com>
2025-01-16 04:34:10 +0700

    lora

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Lora } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
+const lora = Lora({
   subsets: ["latin"],
-  variable: "--font-inter",
+  variable: "--font-lora",
 });
 
 export const metadata: Metadata = {
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${inter.variable} font-sans text-white`}>
+      <body className={`${lora.variable} font-sans text-white`}>
         {children}
       </body>
     </html>
