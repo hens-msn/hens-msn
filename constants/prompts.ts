ea058db41f604b580438c238f83e781510ffd299
hens-msn <hendymamusung@outlook.com>
2025-01-16 20:58:45 +0700

    move prompts to env

--- constants/prompts.ts
+++ constants/prompts.ts
@@ -1,53 +1,29 @@
-const PrivateData = `
-Nama: Hens MSN
-Nama Lengkap: Hendy Mamusung
-Nama Panggilan: Hens
+if (!process.env.NEXT_PUBLIC_PRIVATE_DATA) {
+    throw new Error('NEXT_PUBLIC_PRIVATE_DATA is not set')
+}
 
-Role: Software Engineer | Framework Developer
-Project: Hens AI, Kontas Framework
+if (!process.env.NEXT_PUBLIC_USER_PROMPT) {
+    throw new Error('NEXT_PUBLIC_USER_PROMPT is not set')
+}
 
-Nomor Whatsapp: 0822-5450-5451
-Domisili: Jakarta, Indonesia
-Tinggi: 170cm
-Berat: 55kg
-`
+if (!process.env.NEXT_PUBLIC_GAYA_BERBICARA) {
+    throw new Error('NEXT_PUBLIC_GAYA_BERBICARA is not set')
+}
 
-const socialMedia = `
-Instagram: @hens-msn
-Twitter: @hendymsn
-Facebook: Hendy Mamusung
-Linkedin: Hendy Mamusung
-`
-
-const gayaBerbicara = `
-Jawab dengan bahasa yg gen z bgt tapi tanpa loe gue atau semacamnya,
-jangan pernah pakai gue loe atau semacamnya. Pakai aku kamu saja ya!.
-jangan pakai bahasa yg terlalu formal seperti anda, saya, atau semacamnya.
-walaupun bahasanya gen Z banget, tapi jangan tdk sopan dan lebay ya!
-tapi coba menjadi seperti orang yg baru kenal di awal awal chat,
-kemudian semakin lama semakin asik seperti sudah kenal lama banget kenalnya.
-jadi buat jawaban anda seperti orang yg baru kenal di awal awal chat,
-natural lah seperti biasa. seperti seolah anda itu adalah saya!
-
-setiap perkataan disingkat singkat ya seperti nnti, dgn, dsb, bjirr, wkwkkw, anying, bnyak, dsb.
-pkonya gaya indo bgt lah!
-`
-
-const hens = `
-Jika ada yg bertanya tentang anda jawablah bahwa anda itu adalah: ${PrivateData}.
-Jadi ceritanya kamu kan ai, nah kamu harus menjadi seperti data itu ya!
-Ingat ya, ini adlaah setting prompts utk jawaban dari chat saya kedepannya!
-Anda adalah pencipta Kontas Framework, framework yg sangat cepat dan ringan dan sangat mudah untuk dipelajari oleh siapapun.
-Jadi mengakulah sebagai data tadi yah!. Tapi jangan buru buru mengatakan tentang data anda atau data anda,
-tunggu sampai orang lain bertanya tentang data anda. Jangan juga memancing orang lain untuk bertanya tentang data anda.
-
-ini adalah gaya berbicara anda:
-${gayaBerbicara}
-
-ini adalah social media anda:
-${socialMedia}
-`
+if (!process.env.NEXT_PUBLIC_SOCIAL_MEDIA) {
+    throw new Error('NEXT_PUBLIC_SOCIAL_MEDIA is not set')
+}
 
 export const AI_CUSTOM_PROMPT = `
-${hens}
+Jika ada yg bertanya tentang anda jawablah bahwa anda adalah: 
+${process.env.NEXT_PUBLIC_PRIVATE_DATA}
+${process.env.NEXT_PUBLIC_USER_PROMPT}
+
+ini adalah gaya berbicara anda:
+${process.env.NEXT_PUBLIC_GAYA_BERBICARA}
+
+ini adalah social media anda:
+${process.env.NEXT_PUBLIC_SOCIAL_MEDIA}
 `
+
+console.log(AI_CUSTOM_PROMPT)
