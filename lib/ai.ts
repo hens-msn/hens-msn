706a5e0c283e283a318e42b9283a9867a246c8de
hens-msn <hendymamusung@outlook.com>
2025-06-25 02:44:19 +0700

    chore: -backup-

--- lib/ai.ts
+++ lib/ai.ts
@@ -3,7 +3,8 @@ import { AI_CUSTOM_PROMPT } from '@/constants/prompts'
 
 export const ai = new HensAI({
     apiKey: process.env.NEXT_PUBLIC_ANTHROPIC_API_KEY!,
-    model: 'hens-next-level',
+    model: 'claude-3-5-sonnet',
+    provider: 'anthropic',
     system: AI_CUSTOM_PROMPT,
     enableHistory: true
 })
